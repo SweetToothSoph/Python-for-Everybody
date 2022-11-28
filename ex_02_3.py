ce8a13e39bfc007bd9b477d9073c68dd91643bc5
Sophia <114967357+SweetToothSoph@users.noreply.github.com>
2022-11-28 13:10:00 -0500

    Update ex_02_3.py

--- ex_02_3.py
+++ ex_02_3.py
@@ -1,3 +1,8 @@
+# Exercise 4: Assume that we execute the following assignment state-ments:
+# width = 17
+# height = 12.0
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression).
+
 width = 17
 height = 12.0
 
