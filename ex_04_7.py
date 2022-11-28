173e0f3668722a48bda6337aaad8b2a9456821e9
Sophia <114967357+SweetToothSoph@users.noreply.github.com>
2022-11-28 13:14:50 -0500

    Update ex_04_7.py

--- ex_04_7.py
+++ ex_04_7.py
@@ -1,3 +1,6 @@
+# Exercise 7: Rewrite the grade program from the previous chapter using a function called 
+# computegrade that takes a score as its parameter and returns a grade as a string.
+
 def computegrade(sf):
     if sf > 1.0:
         print("Bad score")
