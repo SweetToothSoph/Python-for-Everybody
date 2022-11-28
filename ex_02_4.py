732f8dafd33be3b126d8a1ae796872a9de848e52
Sophia <114967357+SweetToothSoph@users.noreply.github.com>
2022-11-28 13:10:37 -0500

    Update ex_02_4.py

--- ex_02_4.py
+++ ex_02_4.py
@@ -1,3 +1,6 @@
+# Exercise 5: Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, 
+# and print out the converted temperature.
+
 celsius = 0
 fahrenheit = 32
 temperature = (23 * 9/5) + 32
