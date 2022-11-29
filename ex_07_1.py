5b41bb3fb2a234e5b37cbce349a56c1f22f8e3ef
Sophia <114967357+SweetToothSoph@users.noreply.github.com>
2022-11-28 23:50:25 -0500

    Update ex_07_1.py

--- ex_07_1.py
+++ ex_07_1.py
@@ -1,3 +1,6 @@
+# Exercise 1: Write a program to read through a file and print the 
+# contents of the file (line by line) all in upper case.
+
 file = open('mbox-short.txt')
 for line in file:
     line1 = line.rstrip()
