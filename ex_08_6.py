c60e9793efc24f142dcd6e843f893d1590464bf1
Sophia <114967357+SweetToothSoph@users.noreply.github.com>
2022-12-04 22:03:14 -0500

    Update ex_08_6.py

--- ex_08_6.py
+++ ex_08_6.py
@@ -6,10 +6,10 @@ numb = list()
 
 while True :
     data = input('Enter a number: ')
-    if data == 'done' :
-        break
+    if data == 'done' : break
     else :
         numb.append(data)
+    value = float(data)
 
 print(numb)
 print('Maximum:', max(numb))
